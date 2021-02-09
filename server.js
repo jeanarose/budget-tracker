c5d9a918335b61660b816322e294d62b2004dd70
Jeana Rose <jr.mathis608@gmail.com>
2021-02-09 16:13:20 -0500

    Added process environment variables

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 // TODO: Add process.env 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,10 +17,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // TODO: Add environment variable (MONGODB_URI)
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
+
 
 // routes
 app.use(require("./routes/api.js"));
